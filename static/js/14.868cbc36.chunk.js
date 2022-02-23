3d6cf692b62ef46fc0ce0c57d9333c326fb319f0
KurdGhost69 <kurdghost@protonmail.com>
2022-02-24 00:07:06 +0100

    Updates

--- static/js/14.868cbc36.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[14],{93:function(o,e){window.location.replace("http://somewhereelse.com")}}]);
-//# sourceMappingURL=14.868cbc36.chunk.js.map
