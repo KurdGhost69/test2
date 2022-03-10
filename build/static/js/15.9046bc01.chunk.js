eba55fe684de1e5be4387c9f080b5bef7a0c26d8
KurdGhost69 <kurdghost@protonmail.com>
2022-03-10 18:40:58 +0100

    build folder

---
+++ build/static/js/15.9046bc01.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[15],{93:function(o,e){window.location.replace("http://somewhereelse.com")}}]);
+//# sourceMappingURL=15.9046bc01.chunk.js.map
